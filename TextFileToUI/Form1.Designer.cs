7d7a9b00d1d8135ee31af00c8d1c05b1de799d6f
Toni Papić <tpapic21@foi.hr>
2023-04-28 16:25:41 +0200

    UI elements added (#1)

--- TextFileToUI/Form1.Designer.cs
+++ TextFileToUI/Form1.Designer.cs
@@ -28,36 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.browseFileBtn = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.fullNameBox = new System.Windows.Forms.TextBox();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.yearOfBirth = new System.Windows.Forms.TextBox();
+            this.cityLabel = new System.Windows.Forms.Label();
+            this.cityBox = new System.Windows.Forms.TextBox();
+            this.facultyLabel = new System.Windows.Forms.Label();
+            this.facultyBox = new System.Windows.Forms.TextBox();
+            this.roleLabel = new System.Windows.Forms.Label();
+            this.roleBox = new System.Windows.Forms.TextBox();
+            this.specificRoleLabel = new System.Windows.Forms.Label();
+            this.specificAttributeBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // browseFileBtn
             // 
-            this.button1.Location = new System.Drawing.Point(68, 161);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Browse file";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.browseFileBtn.Location = new System.Drawing.Point(301, 415);
+            this.browseFileBtn.Name = "browseFileBtn";
+            this.browseFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.browseFileBtn.TabIndex = 0;
+            this.browseFileBtn.Text = "Browse file";
+            this.browseFileBtn.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // nameLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(12, 45);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(57, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Full Name:";
+            // 
+            // fullNameBox
+            // 
+            this.fullNameBox.Location = new System.Drawing.Point(128, 45);
+            this.fullNameBox.Name = "fullNameBox";
+            this.fullNameBox.Size = new System.Drawing.Size(157, 20);
+            this.fullNameBox.TabIndex = 2;
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.AutoSize = true;
+            this.yearLabel.Location = new System.Drawing.Point(12, 90);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(67, 13);
+            this.yearLabel.TabIndex = 1;
+            this.yearLabel.Text = "Year of birth:";
+            // 
+            // yearOfBirth
+            // 
+            this.yearOfBirth.Location = new System.Drawing.Point(128, 90);
+            this.yearOfBirth.Name = "yearOfBirth";
+            this.yearOfBirth.Size = new System.Drawing.Size(157, 20);
+            this.yearOfBirth.TabIndex = 2;
+            // 
+            // cityLabel
+            // 
+            this.cityLabel.AutoSize = true;
+            this.cityLabel.Location = new System.Drawing.Point(12, 139);
+            this.cityLabel.Name = "cityLabel";
+            this.cityLabel.Size = new System.Drawing.Size(24, 13);
+            this.cityLabel.TabIndex = 1;
+            this.cityLabel.Text = "City";
+            // 
+            // cityBox
+            // 
+            this.cityBox.Location = new System.Drawing.Point(128, 139);
+            this.cityBox.Name = "cityBox";
+            this.cityBox.Size = new System.Drawing.Size(157, 20);
+            this.cityBox.TabIndex = 2;
+            // 
+            // facultyLabel
+            // 
+            this.facultyLabel.AutoSize = true;
+            this.facultyLabel.Location = new System.Drawing.Point(12, 188);
+            this.facultyLabel.Name = "facultyLabel";
+            this.facultyLabel.Size = new System.Drawing.Size(41, 13);
+            this.facultyLabel.TabIndex = 1;
+            this.facultyLabel.Text = "Faculty";
+            // 
+            // facultyBox
+            // 
+            this.facultyBox.Location = new System.Drawing.Point(128, 185);
+            this.facultyBox.Name = "facultyBox";
+            this.facultyBox.Size = new System.Drawing.Size(157, 20);
+            this.facultyBox.TabIndex = 2;
+            // 
+            // roleLabel
+            // 
+            this.roleLabel.AutoSize = true;
+            this.roleLabel.Location = new System.Drawing.Point(12, 237);
+            this.roleLabel.Name = "roleLabel";
+            this.roleLabel.Size = new System.Drawing.Size(32, 13);
+            this.roleLabel.TabIndex = 1;
+            this.roleLabel.Text = "Role:";
+            // 
+            // roleBox
+            // 
+            this.roleBox.Location = new System.Drawing.Point(128, 234);
+            this.roleBox.Name = "roleBox";
+            this.roleBox.Size = new System.Drawing.Size(157, 20);
+            this.roleBox.TabIndex = 2;
+            // 
+            // specificRoleLabel
+            // 
+            this.specificRoleLabel.AutoSize = true;
+            this.specificRoleLabel.Location = new System.Drawing.Point(12, 294);
+            this.specificRoleLabel.Name = "specificRoleLabel";
+            this.specificRoleLabel.Size = new System.Drawing.Size(110, 13);
+            this.specificRoleLabel.TabIndex = 1;
+            this.specificRoleLabel.Text = "[role specific attribute]";
+            // 
+            // specificAttributeBox
+            // 
+            this.specificAttributeBox.Location = new System.Drawing.Point(128, 291);
+            this.specificAttributeBox.Name = "specificAttributeBox";
+            this.specificAttributeBox.Size = new System.Drawing.Size(157, 20);
+            this.specificAttributeBox.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(388, 450);
+            this.Controls.Add(this.specificAttributeBox);
+            this.Controls.Add(this.specificRoleLabel);
+            this.Controls.Add(this.roleBox);
+            this.Controls.Add(this.roleLabel);
+            this.Controls.Add(this.facultyBox);
+            this.Controls.Add(this.facultyLabel);
+            this.Controls.Add(this.cityBox);
+            this.Controls.Add(this.cityLabel);
+            this.Controls.Add(this.yearOfBirth);
+            this.Controls.Add(this.yearLabel);
+            this.Controls.Add(this.fullNameBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.browseFileBtn);
             this.Name = "Form1";
             this.Text = "File";
             this.ResumeLayout(false);
@@ -67,8 +175,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button browseFileBtn;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox fullNameBox;
+        private System.Windows.Forms.Label yearLabel;
+        private System.Windows.Forms.TextBox yearOfBirth;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.TextBox cityBox;
+        private System.Windows.Forms.Label facultyLabel;
+        private System.Windows.Forms.TextBox facultyBox;
+        private System.Windows.Forms.Label roleLabel;
+        private System.Windows.Forms.TextBox roleBox;
+        private System.Windows.Forms.Label specificRoleLabel;
+        private System.Windows.Forms.TextBox specificAttributeBox;
     }
 }
 
--- TextFileToUI/Form1.cs
+++ TextFileToUI/Form1.cs
@@ -18,24 +18,15 @@ namespace TextFileToUI
     public partial class Form1 : Form
     {
         UserFileReading userFile;
-        UserParser userParser;
+      
         public Form1()
         {
             InitializeComponent();
             userFile = new UserFileReading();
-           
+            
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-
-            userFile.ChooseFile(this);
-            userParser = new UserParser(userFile.ReadFile());
-            label1.Text = userParser.SortData().Ime;
-
-
-        }
+    
     }
 
     public class UserFileReading
@@ -66,67 +57,9 @@ namespace TextFileToUI
         
     }
 
-    public class UserParser
-    {
-        User user;
-        string file = null;
-        public UserParser(string file) {
-        user=new User();
-            this.file = file;
-        
-        }
-
-        public User SortData()
-        {
-
-            
-
-            try { 
-            user.Ime = file.Substring(file.IndexOf("Ime") + 4, (file.IndexOf("Prezime"))-(file.IndexOf("Ime")+4));
-            user.Prezime = file.Substring(file.IndexOf("Prezime") + 8, file.IndexOf("Godina rođenja")-(file.IndexOf("Prezime") + 8));
-            user.GodinaRodjenja= int.Parse(file.Substring(file.IndexOf("Godina rođenja") + 15, file.IndexOf("Grad rođenja")-(file.IndexOf("Godina rođenja") + 15)));
-            user.GradRodjenja = file.Substring(file.IndexOf("Grad rođenja") + 13, file.IndexOf("Fakultet") - ((file.IndexOf("Grad rođenja") + 13)));
-            user.Fakultet = file.Substring(file.IndexOf("Fakultet") + "Fakultet".Length+1, file.IndexOf("Uloga") - (file.IndexOf("Fakultet") + "Fakultet".Length + 1));
+   
 
 
-                if (file.Contains("Najdraži kolegij"))
-                {
-                    user.Uloga= file.Substring(file.IndexOf("Uloga") + "Uloga".Length + 1, file.IndexOf("Najdraži kolegij") - (file.IndexOf("Uloga") + "Uloga".Length + 1));
-                    user.NajdraziKolegij = file.Substring(file.IndexOf("Najdraži kolegij") + "Najdraži kolegij".Length + 1, file.IndexOf("Datoteci pristupljeno puta") - (file.IndexOf("Najdraži kolegij") + "Najdraži kolegij".Length + 1));
-                   
-                }
-                else
-                {
-
-                }
-
-                return user;
-            }
-
-            catch
-            {
-                MessageBox.Show("Doslo je do greske", "Greska", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-                return null;
-            }
 
 
-            
-
-
-        }
-
-    }
-
-    public class User
-    {
-        public string Ime { get; set; }
-        public string Prezime { get; set; }
-        public string GradRodjenja { get; set; }
-        public int GodinaRodjenja { get; set; }
-        public string Fakultet { get; set; }
-        public string Uloga { get; set; }
-        public string NajdraziKolegij { get; set; } 
-        public string Katedra { get; set; }
-    }
 }
