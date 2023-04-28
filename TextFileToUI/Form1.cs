b1140f4c4d3b8045e636a0cec50db54023ef1cd2
Toni Papić <tpapic21@foi.hr>
2023-04-28 19:04:00 +0200

    Add 2 classes (UserParser, User). UserParser loads file data into User object. (#3)

--- TextFileToUI/Form1.cs
+++ TextFileToUI/Form1.cs
@@ -32,8 +32,6 @@ namespace TextFileToUI
     public class UserFileReading
     {
         OpenFileDialog ofd;
-      
-
 
         public UserFileReading()
         {
@@ -57,7 +55,70 @@ namespace TextFileToUI
         
     }
 
-   
+    public class UserParser
+    {
+        User user;
+        string file = null;
+        public UserParser(string file)
+        {
+            user = new User();
+            this.file = file;
+
+        }
+
+        public User SortData()
+        {
+
+            try
+            {
+                user.Ime = file.Substring(file.IndexOf("Ime") + 4, (file.IndexOf("Prezime")) - (file.IndexOf("Ime") + 4));
+                user.Prezime = file.Substring(file.IndexOf("Prezime") + 8, file.IndexOf("Godina rođenja") - (file.IndexOf("Prezime") + 8));
+                user.GodinaRodjenja = int.Parse(file.Substring(file.IndexOf("Godina rođenja") + 15, file.IndexOf("Grad rođenja") - (file.IndexOf("Godina rođenja") + 15)));
+                user.GradRodjenja = file.Substring(file.IndexOf("Grad rođenja") + 13, file.IndexOf("Fakultet") - ((file.IndexOf("Grad rođenja") + 13)));
+                user.Fakultet = file.Substring(file.IndexOf("Fakultet") + "Fakultet".Length + 1, file.IndexOf("Uloga") - (file.IndexOf("Fakultet") + "Fakultet".Length + 1));
+
+
+                if (file.Contains("Najdraži kolegij"))
+                {
+                    user.Uloga = file.Substring(file.IndexOf("Uloga") + "Uloga".Length + 1, file.IndexOf("Najdraži kolegij") - (file.IndexOf("Uloga") + "Uloga".Length + 1));
+                    user.NajdraziKolegij = file.Substring(file.IndexOf("Najdraži kolegij") + "Najdraži kolegij".Length + 1, file.IndexOf("Datoteci pristupljeno puta") - (file.IndexOf("Najdraži kolegij") + "Najdraži kolegij".Length + 1));
+
+                }
+                else
+                {
+                    user.Uloga = file.Substring(file.IndexOf("Uloga") + "Uloga".Length + 1, file.IndexOf("Katedra") - (file.IndexOf("Uloga") + "Uloga".Length + 1));
+                    user.Katedra = file.Substring(file.IndexOf("Katedra") + "Katedra".Length + 1, file.IndexOf("Datoteci pristupljeno puta") - (file.IndexOf("Katedra") + "Katedra".Length + 1));
+
+                }
+
+                return user;
+            }
+
+            catch
+            {
+                MessageBox.Show("Doslo je do greske", "Greska", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return null;
+            }
+
+        }
+
+    }
+
+    public class User
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string GradRodjenja { get; set; }
+        public int GodinaRodjenja { get; set; }
+        public string Fakultet { get; set; }
+        public string Uloga { get; set; }
+        public string NajdraziKolegij { get; set; }
+        public string Katedra { get; set; }
+    }
+
+
+
 
 
 
